baf617070d5c815090fd2c9f59c35eb899b65ee8
czt <312008981@qq.com>
2021-11-14 10:51:22 +0800

    init

---
+++ packages/jest-transform-class/src/class.js
@@ -0,0 +1,16 @@
+class Bork {
+
+    //Property initializer syntax
+    instanceProperty = "bork";
+    boundFunction = () => {
+      return this.instanceProperty;
+    }
+ 
+    //Static class properties
+    static staticProperty = "babelIsCool";
+    static staticFunction = function() {
+      return Bork.staticProperty;
+    }
+  }
+
+  module.exports = Bork;
