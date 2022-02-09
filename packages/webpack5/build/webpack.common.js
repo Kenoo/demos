f6e34eb52f01c81eaf2aa525df0e7f1195e5bd51
czt <312008981@qq.com>
2022-02-09 14:35:25 +0800

    #add loader and dev server

--- packages/webpack5/build/webpack.common.js
+++ packages/webpack5/build/webpack.common.js
@@ -16,6 +16,42 @@ module.exports = {
       format: `  :msg [:bar] ${chalk.green.bold(":percent")} (:elapsed s)`,
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], //从右向左解析
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/, //加载图片资源
+        type: "asset",
+        generator: {
+          filename: "static/img/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,//加载视频资源
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/media/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, //加载字体资源
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/fonts/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     path:path.resolve(__dirname,'../dist'),
     filename: '[name].bundle.js',
--- packages/webpack5/src/index.js
+++ packages/webpack5/src/index.js
@@ -1,1 +1,6 @@
+import './assets/styles/main.css'
 console.log('Interesting!')
+let fn = ()=> {
+    console.log(444);
+}
+fn();
