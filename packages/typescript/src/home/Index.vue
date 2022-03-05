08ac36b7a701127b8689202adae9d2362d06ac83
czt <312008981@qq.com>
2022-03-05 10:08:31 +0800

    # add typescript project

---
+++ packages/typescript/src/home/Index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+function fn(n: number) {
+  if (n > 5) {
+    return true;
+  } else {
+    return false;
+  }
+  return true;
+}
+</script>
+
+<template>
+  <span>czt</span>
+</template>
+
+<style scoped>
+
+</style>
